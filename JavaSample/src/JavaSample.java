1ce01b9ab8dcd337f8e412bb4640e1fba20e1039
Gopu Packirisamy <gopu13@gmail.com>
2019-05-21 15:57:51 -0700

    Commit for initial program

---
+++ JavaSample/src/JavaSample.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author gopupackirisamy
+ *
+ */
+public class JavaSample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Java Git World");
+
+	}
+
+}
